5ccc9d6c61e2e1b435f8d52df18af0b1032b14f9
n1nja00 <ericmbogo2547@gmail.com>
2023-08-16 15:16:28 -0700

    I don't care for circumstances,I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,24 +11,24 @@
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-   int i = 0;
+	int i = 0;
 
 
-   if (size > 0)
-   {
-       if (array != NULL && cmp != NULL)
-       {
-           while (i < size)
-           {
-               if (cmp(array[i]))
-                   return (i);
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
 
 
-               i++;
-           }
-       }
-   }
+				i++;
+			}
+		}
+	}
 
 
-   return (-1);
+	return (-1);
 }
